cdd602dd860b571acda1008f12b2850b0efb69c1
Batikan Turkmen <batikant@gmail.com>
2013-08-22 10:45:31 +0300

    Collection and Topic test except Latchs

--- hazelcast/test/ClientListTest.h
+++ hazelcast/test/ClientListTest.h
@@ -15,7 +15,7 @@
 #include "HazelcastInstance.h"
 #include "hazelcast/client/ClientConfig.h"
 #include "iTest.h"
-#include "IList.h"
+#include "Ilist.h"
 
 using namespace hazelcast::client;
 using namespace std;
@@ -30,8 +30,13 @@ public:
     };
     
     void addTests() {
-        //addTest(&ClientLockTest::testLock, "testLock");
-        
+        addTest(&ClientListTest::testAddAll, "testAddAll");
+        addTest(&ClientListTest::testAddSetRemove, "testAddSetRemove");
+        addTest(&ClientListTest::testIterator, "testIterator");
+        addTest(&ClientListTest::testContains, "testContains");
+        addTest(&ClientListTest::removeRetainAll, "removeRetainAll");
+        addTest(&ClientListTest::testListener, "testListener");
+
     };
     
     void beforeClass() {
@@ -39,7 +44,7 @@ public:
         clientConfig.addAddress(Address("localhost", 5701));
         clientConfig.getGroupConfig().setName("dev").setPassword("dev-pass");
         client = new HazelcastClient(clientConfig);
-        list = new IList(client->getIList("ClientListTest"));
+        list = new IList(client->getList("ClientListTest"));
     };
     
     void afterClass() {
@@ -56,14 +61,34 @@ public:
     };
     
     void testAddAll() {
+        /*List l = new ArrayList();
+        l.add("item1");
+        l.add("item2");
+        
+        assertTrue(list->addAll(l));
+        assertEqual(2, list->size());
+        
+        assertTrue(list->addAll(1, l));
+        assertEqual(4, list->size());*/
+        std::vector<std::string> l;
+        l.push_back("item1");
+        l.push_back("item2");
+        assertTrue(list.addAll(l));
+        
+        
+        assertEqual("item1", list->get(0));
+        assertEqual("item1", list->get(1));
+        assertEqual("item2", list->get(2));
+        assertEqual("item2", list->get(3));
+    }
+    
+    void testAddSetRemove() {
         assertTrue(list->add("item1"));
         assertTrue(list->add("item2"));
         list->add(0,"item3");
         assertEqual(3, list->size());
-        
-        string temp;
-        temp = list->set(2, "item4")
-        assertEqual("item2",temp);
+        string temp = list->set(2, "item4");
+        assertEqual("item2", temp);
         
         assertEqual(3, list->size());
         assertEqual("item3", list->get(0));
@@ -73,12 +98,104 @@ public:
         assertFalse(list->remove("item2"));
         assertTrue(list->remove("item3"));
         
-        string temp1;
+        temp = list->remove(1);
+        assertEqual("item4", temp);
         
-        temp1 = list.remove(1);
-        assertEquals("item4", temp1);
+        assertEqual(1, list->size());
+        assertEqual("item1", list->get(0));
+    }
+   
+   void testIndexOf(){
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item2"));
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item4"));
+        
+        assertEqual(-1, list->indexOf("item5"));
+        assertEqual(0, list->indexOf("item1"));
+        
+        assertEqual(-1, list->lastIndexOf("item6"));
+        assertEqual(2, list->lastIndexOf("item1"));
     }
     
+    void testToArray(){
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item2"));
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item4"));
+        
+        std::vector<std::string> ar = list->toArray();
+        
+        list->subList(1,3);
+        
+        /*Iterator iter = list->iterator();
+        assertEqual("item1",iter.next());
+        assertEqual("item2",iter.next());
+        assertEqual("item1",iter.next());
+        assertEqual("item4",iter.next());
+        assertFalse(iter.hasNext());
+        
+        ListIterator listIterator = list->listIterator(2);
+        assertEqual("item1",listIterator.next());
+        assertEqual("item4",listIterator.next());
+        assertFalse(listIterator.hasNext());
+        
+        List l = list->subList(1, 3);
+        assertEqual(2, l.size());
+        assertEqual("item2", l.get(0));
+        assertEqual("item1", l.get(1));*/
+    }
+   
+   void testContains(){
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item2"));
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item4"));
+        
+        assertFalse(list->contains("item3"));
+        assertTrue(list->contains("item2"));
+        
+       /* List l = new ArrayList();
+        l.add("item4");
+        l.add("item3");
+        
+        assertFalse(list->containsAll(l));
+        assertTrue(list->add("item3"));
+        assertTrue(list->containsAll(l));*/
+    }
+
+    void removeRetainAll(){
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item2"));
+        assertTrue(list->add("item1"));
+        assertTrue(list->add("item4"));
+        
+        /*List l = new ArrayList();
+        l.add("item4");
+        l.add("item3");
+        
+        assertTrue(list->removeAll(l));
+        assertEqual(3, list->size());
+        assertFalse(list->removeAll(l));
+        assertEqual(3, list->size());
+        
+        l.clear();
+        l.add("item1");
+        l.add("item2");
+        assertFalse(list->retainAll(l));
+        assertEqual(3, list->size());
+        
+        l.clear();
+        assertTrue(list->retainAll(l));
+        assertEqual(0, list->size());*/
+        
+    }
+    
+    
+    void testListener() {
+        // Method contains countdownlatch
+        
+    }
     
 private:
     HazelcastClient *client;
---
+++ hazelcast/test/ClientSetTest.h
@@ -0,0 +1,156 @@
+//
+//  ClientSetTest.h
+//  hazelcast
+//
+//  Created by Batikan Turkmen on 16.08.2013.
+//  Copyright (c) 2013 Batikan Turkmen. All rights reserved.
+//
+
+#ifndef hazelcast_ClientSetTest_h
+#define hazelcast_ClientSetTest_h
+
+#include "hazelcast/client/HazelcastClient.h"
+#include "hazelcast/util/Util.h"
+#include "HazelcastInstance.h"
+#include "hazelcast/client/ClientConfig.h"
+#include "iTest.h"
+#include "Iset.h"
+
+class ClientMultiMapTest: public iTestFixture<ClientMultiMapTest>{
+    
+public:
+    
+    ClientMultiMapTest(){
+        
+    };
+    
+    void addTests() {
+        addTest(&ClientMultiMapTest::testAddAll, "testAddAll");
+        addTest(&ClientMultiMapTest::testAddRemove, "testAddRemove");
+        addTest(&ClientMultiMapTest::testIterator, "testIterator");
+        addTest(&ClientMultiMapTest::testContains, "testContains");
+        addTest(&ClientMultiMapTest::removeRetainAll, "removeRetainAll");
+        addTest(&ClientMultiMapTest::testListener, "testListener");
+
+    };
+    
+    void beforeClass() {
+        ClientConfig clientConfig;
+        clientConfig.addAddress(Address("localhost", 5701));
+        clientConfig.getGroupConfig().setName("dev").setPassword("dev-pass");
+        client = new HazelcastClient(clientConfig);
+        set = new ISet(client->getSet("ClientMultiMapTest"));
+    };
+    
+    void afterClass() {
+        delete client;
+        delete set;
+    };
+    
+    void beforeTest() {
+        //Empty
+    };
+    
+    void afterTest() {
+        set->clear();
+    };
+
+   
+   void testAddAll() {
+        /*List l = new ArrayList();
+        l.add("item1");
+        l.add("item2");
+        
+        assertTrue(set->addAll(l));
+        assertEqual(2, set->size());
+        
+        assertFalse(set->addAll(l));*/
+        assertEqual(2, set->size());
+        
+    }
+    
+    void testAddRemove() {
+        assertTrue(set->add("item1"));
+        assertTrue(set->add("item2"));
+        assertTrue(set->add("item3"));
+        assertEqual(3, set->size());
+        
+        assertFalse(set->add("item3"));
+        assertEqual(3, set->size());
+        
+        
+        assertFalse(set->remove("item4"));
+        assertTrue(set->remove("item3"));
+        
+    }
+    
+    void testIterator(){
+        assertTrue(set->add("item1"));
+        assertTrue(set->add("item2"));
+        assertTrue(set->add("item3"));
+        assertTrue(set->add("item4"));
+        
+       /* Iterator iter = set->iterator();
+        assertTrue(((String)iter.next()).startsWith("item"));
+        assertTrue(((String)iter.next()).startsWith("item"));
+        assertTrue(((String)iter.next()).startsWith("item"));
+        assertTrue(((String)iter.next()).startsWith("item"));
+        assertFalse(iter.hasNext());*/
+    }
+    
+    void testContains(){
+        assertTrue(set->add("item1"));
+        assertTrue(set->add("item2"));
+        assertTrue(set->add("item3"));
+        assertTrue(set->add("item4"));
+        
+        assertFalse(set->contains("item5"));
+        assertTrue(set->contains("item2"));
+        
+        /*List l = new ArrayList();
+        l.add("item6");
+        l.add("item3");
+        
+        assertFalse(set->containsAll(l));
+        assertTrue(set->add("item6"));
+        assertTrue(set->containsAll(l));*/
+    }
+    
+    
+    void removeRetainAll(){
+        assertTrue(set->add("item1"));
+        assertTrue(set->add("item2"));
+        assertTrue(set->add("item3"));
+        assertTrue(set->add("item4"));
+        
+        /*List l = new ArrayList();
+        l.add("item4");
+        l.add("item3");
+        
+        assertTrue(set->removeAll(l));
+        assertEqual(2, set->size());
+        assertFalse(set->removeAll(l));
+        assertEqual(2, set->size());
+        
+        l.clear();
+        l.add("item1");
+        l.add("item2");
+        assertFalse(set->retainAll(l));
+        assertEqual(2, set->size());
+        
+        l.clear();*/
+        assertTrue(set->retainAll(l));
+        assertEqual(0, set->size());
+        
+    }
+    
+   
+    void testListener() {
+        //Contain latch
+    }
+
+private:
+    HazelcastClient *client;
+    IList *set;
+
+#endif
