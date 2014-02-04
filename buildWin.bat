4743e1b57663b8ed2b44689499bf9506ba4326e0
mustafa sancar koyunlu <sancar@hazelcast.com>
2014-02-04 19:10:03 +0200

    add missing libraries for 64 bit release in windows. Easy way to configure the build is still necessary

--- buildWin.bat
+++ buildWin.bat
@@ -2,17 +2,17 @@ mkdir build32
 cd .\build32
 cd
 cmake ..
-MSBuild.exe HazelcastClient.sln /clp:NoSummary;NoItemAndPropertyList;ErrorsOnly /nologo
+::MSBuild.exe HazelcastClient.sln /clp:NoSummary;NoItemAndPropertyList;ErrorsOnly /nologo
 
 cd ..
 
-::mkdir build64
-::cd .\build64
-::cd;
-::cmake .. -G "Visual Studio 10 Win64"
-::MSBuild.exe HazelcastClient.sln /w
+mkdir build64
+cd .\build64
+cd;
+cmake .. -G "Visual Studio 10 Win64"
+::MSBuild.exe HazelcastClient.sln /clp:NoSummary;NoItemAndPropertyList;ErrorsOnly /nologo
 
-::cd ..
+cd ..
 
 ::cd java
 ::javac ClientTCPIPListener.java -cp .\hazelcast-3.2-SNAPSHOT.jar
