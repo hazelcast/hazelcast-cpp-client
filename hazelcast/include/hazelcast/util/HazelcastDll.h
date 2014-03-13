dc27b17f59fc31a2fd28597e8ff100dbb056a17a
sancar <sancar@hazelcast.com>
2014-03-13 13:19:36 +0200

    Compile fix related to boost and clang compiler compatibility.

--- hazelcast/include/hazelcast/util/HazelcastDll.h
+++ hazelcast/include/hazelcast/util/HazelcastDll.h
@@ -1,3 +1,7 @@
+#if defined(__SIZEOF_INT128__)
+#  define BOOST_HAS_INT128
+#endif
+
 #if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
 	#ifdef HAZELCAST_EXPORTS
 		#define HAZELCAST_API __declspec(dllexport)
