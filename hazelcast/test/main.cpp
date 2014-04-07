4e854d243f7bc2b973854594b56ba0775a943c38
sancar <sancar@hazelcast.com>
2014-04-07 13:29:27 +0300

    tests

--- hazelcast/test/main.cpp
+++ hazelcast/test/main.cpp
@@ -32,63 +32,63 @@ void unitTests() {
     try {
 
         HazelcastServerFactory factory;
-//
-//        MemberAttributeTest memberAttributeTest(factory);
-//        memberAttributeTest.executeTests();
-//
-//        ClusterTest clusterTest(factory);
-//        clusterTest.executeTests();
-//
-//        ClientSerializationTest serializationTest;
-//        serializationTest.executeTests();
-//
-//        ClientMapTest mapTest(factory);
-//        mapTest.executeTests();
-//
-//        ClientMultiMapTest multiMapTest(factory);
-//        multiMapTest.executeTests();
-//
-//        ClientQueueTest queueTest(factory);
-//        queueTest.executeTests();
-//
-//        ClientListTest listTest(factory);
-//        listTest.executeTests();
-//
-//        ClientSetTest setTest(factory);
-//        setTest.executeTests();
-//
-//        IAtomicLongTest atomTest(factory);
-//        atomTest.executeTests();
-//
-//        IdGeneratorTest generatorTest(factory);
-//        generatorTest.executeTests();
-//
-//        ICountDownLatchTest latchTest(factory);
-//        latchTest.executeTests();
-//
-//        ClientLockTest lockTest(factory);
-//        lockTest.executeTests();
-//
-//        ClientSemaphoreTest semaphoreTest(factory);
-//        semaphoreTest.executeTests();
-//
-//        ClientTopicTest topicTest(factory);
-//        topicTest.executeTests();
-//
-//        ClientTxnListTest clientTxnListTest(factory);
-//        clientTxnListTest.executeTests();
-//
-//        ClientTxnMapTest clientTxnMapTest(factory);
-//        clientTxnMapTest.executeTests();
-//
-//        ClientTxnMultiMapTest clientTxnMultiMapTest(factory);
-//        clientTxnMultiMapTest.executeTests();
-//
-//        ClientTxnQueueTest clientTxnQueueTest(factory);
-//        clientTxnQueueTest.executeTests();
-//
-//        ClientTxnSetTest clientTxnSetTest(factory);
-//        clientTxnSetTest.executeTests();
+
+        MemberAttributeTest memberAttributeTest(factory);
+        memberAttributeTest.executeTests();
+
+        ClusterTest clusterTest(factory);
+        clusterTest.executeTests();
+
+        ClientSerializationTest serializationTest;
+        serializationTest.executeTests();
+
+        ClientMapTest mapTest(factory);
+        mapTest.executeTests();
+
+        ClientMultiMapTest multiMapTest(factory);
+        multiMapTest.executeTests();
+
+        ClientQueueTest queueTest(factory);
+        queueTest.executeTests();
+
+        ClientListTest listTest(factory);
+        listTest.executeTests();
+
+        ClientSetTest setTest(factory);
+        setTest.executeTests();
+
+        IAtomicLongTest atomTest(factory);
+        atomTest.executeTests();
+
+        IdGeneratorTest generatorTest(factory);
+        generatorTest.executeTests();
+
+        ICountDownLatchTest latchTest(factory);
+        latchTest.executeTests();
+
+        ClientLockTest lockTest(factory);
+        lockTest.executeTests();
+
+        ClientSemaphoreTest semaphoreTest(factory);
+        semaphoreTest.executeTests();
+
+        ClientTopicTest topicTest(factory);
+        topicTest.executeTests();
+
+        ClientTxnListTest clientTxnListTest(factory);
+        clientTxnListTest.executeTests();
+
+        ClientTxnMapTest clientTxnMapTest(factory);
+        clientTxnMapTest.executeTests();
+
+        ClientTxnMultiMapTest clientTxnMultiMapTest(factory);
+        clientTxnMultiMapTest.executeTests();
+
+        ClientTxnQueueTest clientTxnQueueTest(factory);
+        clientTxnQueueTest.executeTests();
+
+        ClientTxnSetTest clientTxnSetTest(factory);
+        clientTxnSetTest.executeTests();
 
         ClientTxnTest clientTxnTest(factory);
         clientTxnTest.executeTests();
