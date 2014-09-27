e037ea7efc44dc863644d77b2c65c5cb7c6965fc
Bilal <bilalyasar45@gmail.com>
2014-09-27 20:45:45 +0300

    test script update

---
+++ testLinux.sh
@@ -0,0 +1,14 @@
+set -e
+rm -rf build
+mkdir build
+cd build
+cmake .. -DHZ_LIB_TYPE=STATIC -DHZ_BIT=64 -DCMAKE_BUILD_TYPE=Debug
+make
+cd ..
+cd java
+mvn clean install
+mvn exec:java -Dexec.mainClass="CppClientListener" &
+sleep 20
+cd ..
+./build/hazelcast/test/clientTest_STATIC_64.exe
+#lsof | grep IPv | grep 6543 | awk '{print $2}' | xargs -l -i kill -9 {}
