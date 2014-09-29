a32fa93a367775997e9b643f1e5eba1ac414336e
devopshazelcast <devops@hazelcast.com>
2014-09-29 13:06:21 +0000

    added windows test script

---
+++ testWindows.bat
@@ -0,0 +1,13 @@
+rm -rf build
+mkdir build
+cd build
+cmake .. -G "Visual Studio 12 Win64" -DHZ_LIB_TYPE=STATIC -DHZ_BIT=64 -DCMAKE_BUILD_TYPE=Debug 
+MSBuild.exe HazelcastClient.sln /property:Configuration=Debug /p:VisualStudioVersion=12.0
+cd ..
+cd java
+mvn clean install
+start "cpp-java" mvn exec:java -Dexec.mainClass="CppClientListener"
+ping 1.1.1.1 -n 1 -w 3000 > nul
+cd ..
+build\hazelcast\test\Debug\clientTest_STATIC_64.exe.exe
+taskkill /F /FI "WINDOWTITLE eq cpp-java"
