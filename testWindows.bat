831e02550f90a4e1be8bc363cb61e0a581a7b56f
sancar <sancar@hazelcast.com>
2014-12-18 16:33:01 +0200

    make testWindows script fail when a test fails

--- testWindows.bat
+++ testWindows.bat
@@ -2,11 +2,11 @@ rm -rf build
 mkdir build
 cd build
 cmake .. -G "Visual Studio 12 Win64" -DHZ_LIB_TYPE=STATIC -DHZ_BIT=64 -DCMAKE_BUILD_TYPE=Release
-MSBuild.exe HazelcastClient.sln /property:Configuration=Release /p:VisualStudioVersion=12.0
+MSBuild.exe HazelcastClient.sln /property:Configuration=Release /p:VisualStudioVersion=12.0 || exit /b 1 
 cd ..
 cd java
 start "cpp-java" mvn package exec:java -Dexec.mainClass="CppClientListener"
 ping 1.1.1.1 -n 1 -w 3000 > nul
 cd ..
-build\hazelcast\test\Release\clientTest_STATIC_64.exe.exe
+build\hazelcast\test\Release\clientTest_STATIC_64.exe.exe || exit /b 1
 taskkill /F /FI "WINDOWTITLE eq cpp-java"
