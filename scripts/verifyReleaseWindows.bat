8e1a54a17c85e563a918954dc3f5996c48d97621
ihsan demir <ihsan@hazelcast.com>
2016-02-22 16:54:33 +0200

    Fix for release script error when there is space in directory path.

--- scripts/verifyReleaseWindows.bat
+++ scripts/verifyReleaseWindows.bat
@@ -1,13 +1,13 @@
-# Verify release
+REM Verify release
 SET CURRENT_DIRECTORY=%cd%
 
-call scripts\verifyReleaseWindowsSingleCase.bat %CURRENT_DIRECTORY%\cpp 32 STATIC || exit /b 1
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 32 STATIC || exit /b 1
 
-call scripts\verifyReleaseWindowsSingleCase.bat %CURRENT_DIRECTORY%\cpp 32 SHARED || exit /b 1
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 32 SHARED || exit /b 1
 
-call scripts\verifyReleaseWindowsSingleCase.bat %CURRENT_DIRECTORY%\cpp 64 STATIC || exit /b 1
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 64 STATIC || exit /b 1
 
-call scripts\verifyReleaseWindowsSingleCase.bat %CURRENT_DIRECTORY%\cpp 64 SHARED || exit /b 1
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 64 SHARED || exit /b 1
 
 exit 0
 
