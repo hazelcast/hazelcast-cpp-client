0247568eef5286785b0cc28a7909470bec7cfa1e
ihsan demir <ihsan@hazelcast.com>
2016-04-06 14:37:24 +0300

    InPredicate

---
+++ hazelcast/include/hazelcast/client/query/InPredicate.h
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2008-2015, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef HAZELCAST_CLIENT_QUERY_INPREDICATE_H_
+#define HAZELCAST_CLIENT_QUERY_INPREDICATE_H_
+
+#include <string>
+#include <vector>
+#include "hazelcast/client/serialization/IdentifiedDataSerializable.h"
+#include "hazelcast/client/serialization/ObjectDataOutput.h"
+#include "hazelcast/client/serialization/ObjectDataInput.h"
+#include "hazelcast/client/exception/IException.h"
+#include "hazelcast/client/query/impl/predicates/PredicateDataSerializerHook.h"
+
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(push)
+#pragma warning(disable: 4251) //for dll export
+#endif
+
+namespace hazelcast {
+    namespace client {
+        namespace query {
+            /**
+             * Type T should be a valid serializable and copiable type.
+             */
+            template <typename T>
+            class InPredicate : public serialization::IdentifiedDataSerializable {
+            public:
+                /**
+                 * @param attributeName The attribute whose value shall be compared to.
+                 * @tparam value The values to search for
+                 */
+                InPredicate(const char *attributeName, const std::vector<T> &values)
+                        : attrName(attributeName), inValues(values) {
+                }
+
+                /**
+                 * Value to add to the searched values in the query
+                 */
+                void add(const T &value) {
+                    inValues.push_back(value);
+                }
+
+                /**
+                 * @return factory id
+                 */
+                int getFactoryId() const {
+                    return impl::predicates::F_ID;
+                }
+
+                /**
+                 * @return class id
+                 */
+                int getClassId() const {
+                    return impl::predicates::IN_PREDICATE;
+                }
+
+                /**
+                 * Defines how this class will be written.
+                 * @param writer ObjectDataOutput
+                 */
+                void writeData(serialization::ObjectDataOutput &out) const {
+                    out.writeUTF(&attrName);
+                    int len = (int)inValues.size();
+                    out.writeInt(len);
+                    for (int i = 0; i < len; ++i) {
+                        out.writeObject<T>(&inValues[i]);
+                    }
+                }
+
+                /**
+                 *Defines how this class will be read.
+                 * @param reader ObjectDataInput
+                 */
+                void readData(serialization::ObjectDataInput &in) {
+                    // Not need to read at the client side
+                    throw exception::IException("InPredicate::readData",
+                                                "Client should not need to use readData method!!!");
+                }
+
+            private:
+                std::string attrName;
+                std::vector<T> inValues;
+            };
+        }
+    }
+}
+
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(pop)
+#endif 
+
+#endif /* HAZELCAST_CLIENT_QUERY_INPREDICATE_H_ */
--- hazelcast/test/src/map/ClientMapTest.cpp
+++ hazelcast/test/src/map/ClientMapTest.cpp
@@ -16,6 +16,7 @@
 //
 // Created by sancar koyunlu on 8/27/13.
 
+#include "hazelcast/client/query/InPredicate.h"
 #include "hazelcast/client/query/ILikePredicate.h"
 #include "hazelcast/client/query/LikePredicate.h"
 #include "hazelcast/client/query/GreaterLessPredicate.h"
@@ -898,6 +899,31 @@ namespace hazelcast {
 
                 ASSERT_EQ(1, (int) result.size());
                 ASSERT_EQ(true, (result.end() != result.find(5)));
+            }
+
+            TEST_F(ClientMapTest, testExecuteOnEntriesWithInPredicate) {
+                IMap<int, Employee> employees = client->getMap<int, Employee>("testExecuteOnEntries");
+
+                Employee empl1("ahmet", 35);
+                Employee empl2("mehmet", 21);
+                Employee empl3("deniz", 25);
+
+                employees.put(3, empl1);
+                employees.put(4, empl2);
+                employees.put(5, empl3);
+
+                EntryMultiplier processor(4);
+
+                std::vector<std::string> values;
+                values.push_back("ahmet");
+                query::InPredicate<std::string> predicate("n", values);
+                predicate.add("mehmet");
+                std::map<int, boost::shared_ptr<int> > result = employees.executeOnEntries<int, EntryMultiplier>(
+                        processor, predicate);
+
+                ASSERT_EQ(2, (int) result.size());
+                ASSERT_EQ(true, (result.end() != result.find(3)));
+                ASSERT_EQ(true, (result.end() != result.find(4)));
             }
 
         }
