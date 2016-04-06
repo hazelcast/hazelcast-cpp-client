326a822ef478d627a9b85827a32f08e50e3d2996
ihsan demir <ihsan@hazelcast.com>
2016-04-06 12:47:22 +0300

    EqualPredicate

--- hazelcast/test/src/map/ClientMapTest.cpp
+++ hazelcast/test/src/map/ClientMapTest.cpp
@@ -16,8 +16,9 @@
 //
 // Created by sancar koyunlu on 8/27/13.
 
-#include <hazelcast/client/query/AndPredicate.h>
-#include <hazelcast/client/query/BetweenPredicate.h>
+#include "hazelcast/client/query/AndPredicate.h"
+#include "hazelcast/client/query/BetweenPredicate.h"
+#include "hazelcast/client/query/EqualPredicate.h"
 #include "hazelcast/client/query/TruePredicate.h"
 #include "hazelcast/client/query/FalsePredicate.h"
 #include "hazelcast/client/query/SqlPredicate.h"
@@ -789,6 +790,31 @@ namespace hazelcast {
                 ASSERT_EQ(true, (result.end() != result.find(5)));
                 ASSERT_EQ(3 * processor.getMultiplier(), *result[3]);
                 ASSERT_EQ(5 * processor.getMultiplier(), *result[5]);
+            }
+
+            TEST_F(ClientMapTest, testExecuteOnEntriesWithEqualPredicate) {
+                IMap<int, Employee> employees = client->getMap<int, Employee>("testExecuteOnEntries");
+
+                Employee empl1("ahmet", 35);
+                Employee empl2("mehmet", 21);
+                Employee empl3("deniz", 25);
+
+                employees.put(3, empl1);
+                employees.put(4, empl2);
+                employees.put(5, empl3);
+
+                EntryMultiplier processor(4);
+
+                std::map<int, boost::shared_ptr<int> > result = employees.executeOnEntries<int, EntryMultiplier>(
+                        processor, query::EqualPredicate<int>("a", 25));
+
+                ASSERT_EQ(1, (int) result.size());
+                ASSERT_EQ(true, (result.end() != result.find(5)));
+
+                result = employees.executeOnEntries<int, EntryMultiplier>(
+                        processor, query::EqualPredicate<int>("a", 10));
+
+                ASSERT_EQ(0, (int) result.size());
             }
 
         }
