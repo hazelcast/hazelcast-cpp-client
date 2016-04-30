681d22ddfa90865620e51c1d9249ec4fa5df74e8
ihsan demir <ihsan@hazelcast.com>
2016-04-30 20:09:22 +0300

    Corrected verify release script after th last fix for relase scripts. (#111)

--- scripts/verifyReleaseWindowsSingleCase.bat
+++ scripts/verifyReleaseWindowsSingleCase.bat
@@ -1,8 +1,9 @@
 SET HZ_INSTALL_DIR=%1
+SET HZ_VERSION="3.7-SNAPSHOT"
 SET HZ_BIT_VERSION=%2
 SET HZ_LIB_TYPE=%3
 
-echo "Verifying the release located at %HZ_INSTALL_DIR% for %HZ_LIB_TYPE% %HZ_BIT_VERSION% bit library."
+echo "Verifying the release located at %HZ_INSTALL_DIR% for version %HZ_VERSION% %HZ_LIB_TYPE% %HZ_BIT_VERSION%-bit library."
 
 cd examples
 rm -rf build
@@ -20,13 +21,13 @@ if %HZ_BIT_VERSION% == 32 (
 )
 
 echo "Generating the solution files for compilation"
-cmake .. -G %SOLUTIONTYPE% -DHAZELCAST_INSTALL_DIR=%HZ_INSTALL_DIR% -DHZ_LIB_TYPE=%HZ_LIB_TYPE% -DHZ_BIT=%HZ_BIT_VERSION% -DCMAKE_BUILD_TYPE=%HZ_BUILD_TYPE% || exit /b 1
+cmake .. -G %SOLUTIONTYPE% -DHAZELCAST_INSTALL_DIR=%HZ_INSTALL_DIR% -DHZ_LIB_TYPE=%HZ_LIB_TYPE% -DHZ_VERSION=${HZ_VERSION} -DHZ_BIT=%HZ_BIT_VERSION% -DCMAKE_BUILD_TYPE=%HZ_BUILD_TYPE% || exit /b 1
 
 echo "Building for platform %BUILDFORPLATFORM%"
 
 MSBuild.exe HazelcastExamples.sln /m /p:Flavor=%HZ_BUILD_TYPE%;Configuration=%HZ_BUILD_TYPE%;VisualStudioVersion=12.0;Platform=%BUILDFORPLATFORM%;PlatformTarget=%BUILDFORPLATFORM% || exit /b 1
 
-echo "Verification of the release located at %HZ_INSTALL_DIR% for %HZ_LIB_TYPE% %HZ_BIT_VERSION% bit library is finished."
+echo "Verification of the release located at %HZ_INSTALL_DIR% for version %HZ_VERSION% %HZ_LIB_TYPE% %HZ_BIT_VERSION%-bit library is finished."
 
 exit 0
 
