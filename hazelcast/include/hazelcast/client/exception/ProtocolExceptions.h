08e133352011624d27cce6443baa3b29f2b240fe
ihsan demir <ihsan@hazelcast.com>
2016-05-23 10:21:07 +0300

    Windows portbility fix: Added the warning suppression for warning 4251 dll export.

--- hazelcast/include/hazelcast/client/exception/ProtocolExceptions.h
+++ hazelcast/include/hazelcast/client/exception/ProtocolExceptions.h
@@ -23,6 +23,11 @@
 
 #include "hazelcast/client/exception/IException.h"
 
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(push)
+#pragma warning(disable: 4251) //for dll export
+#endif
+
 namespace hazelcast {
     namespace client {
         namespace exception {
@@ -146,5 +151,9 @@ namespace hazelcast {
         }
     }
 }
+
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(pop)
+#endif
 
 #endif //HAZELCAST_CLIENT_EXCEPTION_PROTOCOLEXCEPTIONS_H_
--- hazelcast/include/hazelcast/client/protocol/ClientExceptionFactory.h
+++ hazelcast/include/hazelcast/client/protocol/ClientExceptionFactory.h
@@ -21,6 +21,11 @@
 #include <map>
 #include <stdint.h>
 
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(push)
+#pragma warning(disable: 4251) //for dll export
+#endif
+
 namespace hazelcast {
     namespace client {
         namespace exception {
@@ -52,5 +57,9 @@ namespace hazelcast {
         }
     }
 }
+
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(pop)
+#endif
 
 #endif // HAZELCAST_CLIENT_PROTOCOL_CLIENTEXCEPTIONFACTORY_H_
