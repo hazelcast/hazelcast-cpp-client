000226be7a12115286fc5cc371217713d317409c
ihsan demir <ihsan@hazelcast.com>
2017-01-25 18:56:57 +0300

    fix

--- runAllNearCacheTests.sh
+++ runAllNearCacheTests.sh
@@ -17,42 +17,42 @@ DURATION=600000
 NUM_THREADS=4
 KEY_SET_SIZE=10000
 OPERATION_INTERVAL=1
-OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
 USE_NEAR_CACHE="--use-near-cache"
+OUT_FILE=${NUMBER_OF_MEMBERS}member${USE_NEAR_CACHE}${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads.txt
 
-${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP} --operation-interval-in-millis=${OPERATION_INTERVAL}
 
 # TEST CASE: no_miss_without_nearcache_1_member_10000keys_10minutes_1msecInterval_4threads
 DURATION=600000
 NUM_THREADS=4
 KEY_SET_SIZE=10000
 OPERATION_INTERVAL=1
-OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
 USE_NEAR_CACHE=""
+OUT_FILE=${NUMBER_OF_MEMBERS}member${USE_NEAR_CACHE}${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads.txt
 
-${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP} --operation-interval-in-millis=${OPERATION_INTERVAL}
 
 # TEST CASE: no_miss_with_nearcache_1_member_10000keys_10minutes_1msecInterval_40threads
 DURATION=600000
 NUM_THREADS=40
 KEY_SET_SIZE=10000
 OPERATION_INTERVAL=1
-OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
 USE_NEAR_CACHE="--use-near-cache"
+OUT_FILE=${NUMBER_OF_MEMBERS}member${USE_NEAR_CACHE}${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads.txt
 
-${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP} --operation-interval-in-millis=${OPERATION_INTERVAL}
 
 # TEST CASE: no_miss_without_nearcache_1_member_10000keys_10minutes_1msecInterval_40threads
 DURATION=600000
 NUM_THREADS=4
 KEY_SET_SIZE=10000
 OPERATION_INTERVAL=1
-OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
 USE_NEAR_CACHE=""
+OUT_FILE=${NUMBER_OF_MEMBERS}member${USE_NEAR_CACHE}${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads.txt
 
-${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP} --operation-interval-in-millis=${OPERATION_INTERVAL}
 
-echo "All tests finished"
+echo "All C++ tests are finished."
 
 exit 0
 
