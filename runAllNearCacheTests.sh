504bb010efffac3aeb49c844f30d804238f3fea3
ihsan demir <ihsan@hazelcast.com>
2017-01-25 17:41:35 +0300

    Added the script that will run all near cache test scenarios for c++.

---
+++ runAllNearCacheTests.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: runAllNearCacheTests <path of the executable> <server ip address>"
+    exit 1
+fi
+
+TestExecutable=$1
+SERVER_IP=$2
+NUMBER_OF_MEMBERS=1
+
+echo "Testing the executable ${TestExecutable} against server ${SERVER_IP}"
+
+# TEST CASE: no_miss_with_nearcache_1_member_10000keys_10minutes_1msecInterval_4threads
+DURATION=600000
+NUM_THREADS=4
+KEY_SET_SIZE=10000
+OPERATION_INTERVAL=1
+OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
+USE_NEAR_CACHE="--use-near-cache"
+
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+
+# TEST CASE: no_miss_without_nearcache_1_member_10000keys_10minutes_1msecInterval_4threads
+DURATION=600000
+NUM_THREADS=4
+KEY_SET_SIZE=10000
+OPERATION_INTERVAL=1
+OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
+USE_NEAR_CACHE=""
+
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+
+# TEST CASE: no_miss_with_nearcache_1_member_10000keys_10minutes_1msecInterval_40threads
+DURATION=600000
+NUM_THREADS=40
+KEY_SET_SIZE=10000
+OPERATION_INTERVAL=1
+OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
+USE_NEAR_CACHE="--use-near-cache"
+
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+
+# TEST CASE: no_miss_without_nearcache_1_member_10000keys_10minutes_1msecInterval_40threads
+DURATION=600000
+NUM_THREADS=4
+KEY_SET_SIZE=10000
+OPERATION_INTERVAL=1
+OUT_FILE=no_miss_with_nearcache_${NUMBER_OF_MEMBERS}_member_${KEY_SET_SIZE}keys_${DURATION}milliseconds_${OPERATION_INTERVAL}msecInterval_${NUM_THREADS}threads  --operation-interval-in-millis=${OPERATION_INTERVAL}
+USE_NEAR_CACHE=""
+
+${TestExecutable} --test-duration-in-milliseconds=${DURATION} --num-threads=${NUM_THREADS} --stats-output-file=${OUT_FILE}  ${USE_NEAR_CACHE} --key-set-size=${KEY_SET_SIZE} --server-ip=${SERVER_IP}
+
+echo "All tests finished"
+
+exit 0
+
+
