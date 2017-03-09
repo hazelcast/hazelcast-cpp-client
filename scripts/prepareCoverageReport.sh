10cb4a9e51520874373d582772ee36e1d81ebd99
ihsan demir <ihsan@hazelcast.com>
2017-03-09 17:55:37 +0300

    Corrected the gvovr option used for exclusion.

--- scripts/prepareCoverageReport.sh
+++ scripts/prepareCoverageReport.sh
@@ -2,7 +2,7 @@
 
 set -e #abort the script at first failure
 
-gcovr --xml-pretty -o cpp_coverage.xml -r . -e ".*boost.*" -e ".*test.*" -e ".*iTest.*" -e ".*usr.*include.*" --exclude-directories=external/.* -d
+gcovr --xml-pretty -o cpp_coverage.xml -r . -e ".*boost.*" -e ".*test.*" -e ".*iTest.*" -e ".*usr.*include.*" -e ".*asio.*" -d
 
 
 
