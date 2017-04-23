6b358bf3caed06f15c7d7a2d697bb6ac5930c8f4
ihsan demir <ihsan@hazelcast.com>
2017-04-23 19:38:50 +0300

    Disable the HttpsClientTest if tls/ssl is not enabled. (#277)

--- hazelcast/test/src/util/HttpsClientTest.cpp
+++ hazelcast/test/src/util/HttpsClientTest.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifdef HZ_BUILD_WITH_SSL
 
 #include <gtest/gtest.h>
 
@@ -44,3 +45,4 @@ namespace hazelcast {
     }
 }
 
+#endif // HZ_BUILD_WITH_SSL
