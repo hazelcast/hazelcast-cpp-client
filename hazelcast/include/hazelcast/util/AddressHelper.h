5096ad9a25edad25280bb526b1d3cb6afaa66e0c
ihsandemir <ihsandemir@gmail.com>
2018-03-09 23:20:53 +0300

    Windows warning fix.

--- hazelcast/include/hazelcast/util/AddressHelper.h
+++ hazelcast/include/hazelcast/util/AddressHelper.h
@@ -23,6 +23,11 @@
 #include "hazelcast/client/Address.h"
 #include "hazelcast/util/HazelcastDll.h"
 
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(push)
+#pragma warning(disable: 4251) //for dll export
+#endif
+
 namespace hazelcast {
     namespace util {
         /**
@@ -63,6 +68,9 @@ namespace hazelcast {
     }
 }
 
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(pop)
+#endif
 
 #endif //HAZELCAST_UTIL_ADDRESSHELPER_H_
 
