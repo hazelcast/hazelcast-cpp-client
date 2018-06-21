34cfa9ebe1b28103b66e83b24b52020513e823d9
ihsandemir <ihsandemir@gmail.com>
2018-06-21 16:52:40 +0300

    Changed all the release scripts to utilize the same build code as the test scripts. Introduced the common scripts build-linux.sh and build-windows.bat to achieve this.

--- releaseWindows.bat
+++ releaseWindows.bat
@@ -2,52 +2,56 @@ echo "Cleanup release directories"
 for /d %%G in ("Release*") do rd /s /q "%%G"
 rd /s /q cpp
 
-echo "Compiling Static 32bit library"
-mkdir ReleaseStatic32
-cd .\ReleaseStatic32
-cd
-cmake .. -G "Visual Studio 12" -DHZ_LIB_TYPE=STATIC -DHZ_BIT=32  -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=32;Platform=win32;PlatformTarget=win32
-cd ..
-
-echo "Compiling Static 32bit library with TLS support"
-mkdir ReleaseStatic32TLS
-cd .\ReleaseStatic32TLS
-cd
-cmake .. -G "Visual Studio 12" -DHZ_LIB_TYPE=STATIC -DHZ_BIT=32  -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON -DHZ_COMPILE_WITH_SSL=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=32;Platform=win32;PlatformTarget=win32
-cd ..
-
-echo "Compiling Shared 32bit library"
-mkdir ReleaseShared32
-cd .\ReleaseShared32
-cd;
-cmake .. -G "Visual Studio 12" -DHZ_LIB_TYPE=SHARED -DHZ_BIT=32  -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=32;Platform=win32;PlatformTarget=win32
-cd ..
-
-echo "Compiling Shared 32bit library with TLS support"
-mkdir ReleaseShared32TLS
-cd .\ReleaseShared32TLS
-cd;
-cmake .. -G "Visual Studio 12" -DHZ_LIB_TYPE=SHARED -DHZ_BIT=32  -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON -DHZ_COMPILE_WITH_SSL=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=32;Platform=win32;PlatformTarget=win32
-cd ..
-
-echo "Moving 32bit libraries to target"
 mkdir .\cpp\Windows_32\hazelcast\lib\static
 mkdir .\cpp\Windows_32\hazelcast\lib\tls\static
 mkdir .\cpp\Windows_32\hazelcast\lib\shared
 mkdir .\cpp\Windows_32\hazelcast\lib\tls\shared
 mkdir .\cpp\Windows_32\hazelcast\include\hazelcast\
 
+mkdir .\cpp\Windows_64\hazelcast\lib\static
+mkdir .\cpp\Windows_64\hazelcast\lib\tls\static
+mkdir .\cpp\Windows_64\hazelcast\lib\shared
+mkdir .\cpp\Windows_64\hazelcast\lib\tls\shared
+mkdir .\cpp\Windows_64\hazelcast\include\hazelcast
+
 xcopy /S /Q hazelcast\include\hazelcast\* cpp\Windows_32\hazelcast\include\hazelcast\
 xcopy /S /Q hazelcast\generated-sources\include\hazelcast\* cpp\Windows_32\hazelcast\include\hazelcast\
 
-copy ReleaseShared32\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\shared\
-copy ReleaseShared32TLS\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\tls\shared\
-copy ReleaseStatic32\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\static\
-copy ReleaseStatic32TLS\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\tls\static\
+xcopy /S /Q hazelcast\include\hazelcast\* cpp\Windows_64\hazelcast\include\hazelcast\
+xcopy /S /Q hazelcast\generated-sources\include\hazelcast\* cpp\Windows_64\hazelcast\include\hazelcast\
+
+call scripts\build-windows.bat 32 STATIC Release COMPILE_WITHOUT_SSL || exit /b 1
+copy buildSTATIC32Release\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\static\
+rd /s /q buildSTATIC32Release
+
+call scripts\build-windows.bat 32 SHARED Release COMPILE_WITHOUT_SSL || exit /b 1
+copy buildSHARED32Release\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\shared\
+rd /s /q buildSHARED32Release
+
+call scripts\build-windows.bat 64 STATIC Release COMPILE_WITHOUT_SSL || exit /b 1
+copy buildSTATIC64Release\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\static\
+rd /s /q buildSTATIC64Release
+
+call scripts\build-windows.bat 64 SHARED Release COMPILE_WITHOUT_SSL || exit /b 1
+copy buildSHARED64Release\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\shared\
+rd /s /q buildSHARED64Release
+
+echo "Generating TLS enabled libraries"
+call scripts\build-windows.bat 32 STATIC Release || exit /b 1
+copy buildSTATIC32Release\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\tls\static\
+rd /s /q buildSTATIC32Release
+
+call scripts\build-windows.bat 32 SHARED Release || exit /b 1
+copy buildSHARED32Release\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\tls\shared\
+rd /s /q buildSHARED32Release
+
+call scripts\build-windows.bat 64 STATIC Release COMPILE_WITHOUT_SSL || exit /b 1
+copy buildSTATIC64Release\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\tls\static\
+rd /s /q buildSTATIC64Release
+
+call scripts\build-windows.bat 64 SHARED Release COMPILE_WITHOUT_SSL || exit /b 1
+copy buildSHARED64Release\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\tls\shared\
+rd /s /q buildSHARED64Release
 
 echo "Copying external libraries and the examples"
 mkdir cpp\external\include
@@ -61,70 +65,11 @@ mklink /j examples ..\examples
 mklink /j external ..\external
 popd
 
-echo "Clearing temporary 32bit librares"
-rd /s /q .\ReleaseShared32
-rd /s /q .\ReleaseShared32TLS
-rd /s /q .\ReleaseStatic32
-rd /s /q .\ReleaseStatic32TLS
-
-echo "Compiling Static 64bit library"
-mkdir ReleaseStatic64
-cd .\ReleaseStatic64
-cd;
-cmake .. -G "Visual Studio 12 Win64" -DHZ_LIB_TYPE=STATIC -DHZ_BIT=64 -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=64;Platform=x64;PlatformTarget=x64
-cd ..
-
-echo "Compiling Static 64bit library with TLS support"
-mkdir ReleaseStatic64TLS
-cd .\ReleaseStatic64TLS
-cd;
-cmake .. -G "Visual Studio 12 Win64" -DHZ_LIB_TYPE=STATIC -DHZ_BIT=64 -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON -DHZ_COMPILE_WITH_SSL=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=64;Platform=x64;PlatformTarget=x64
-cd ..
-
-echo "Compiling Shared 64bit library"
-mkdir ReleaseShared64
-cd .\ReleaseShared64
-cd;
-cmake .. -G "Visual Studio 12 Win64" -DHZ_LIB_TYPE=SHARED -DHZ_BIT=64 -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=64;Platform=x64;PlatformTarget=x64
-cd ..
-
-echo "Compiling Shared 64bit library with TLS support"
-mkdir ReleaseShared64TLS
-cd .\ReleaseShared64TLS
-cd;
-cmake .. -G "Visual Studio 12 Win64" -DHZ_LIB_TYPE=SHARED -DHZ_BIT=64 -DCMAKE_BUILD_TYPE=Release  -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON -DHZ_COMPILE_WITH_SSL=ON
-MSBuild.exe HazelcastClient.sln /m /property:Configuration=Release /p:VisualStudioVersion=12.0;Flavor=64;Platform=x64;PlatformTarget=x64
-cd ..
-
-echo "Moving 64bit libraries to target"
-mkdir .\cpp\Windows_64\hazelcast\lib\static
-mkdir .\cpp\Windows_64\hazelcast\lib\tls\static
-mkdir .\cpp\Windows_64\hazelcast\lib\shared
-mkdir .\cpp\Windows_64\hazelcast\lib\tls\shared
-mkdir .\cpp\Windows_64\hazelcast\include\hazelcast
-
-xcopy /S /Q hazelcast\include\hazelcast\* cpp\Windows_64\hazelcast\include\hazelcast\
-xcopy /S /Q hazelcast\generated-sources\include\hazelcast\* cpp\Windows_64\hazelcast\include\hazelcast\
-
-copy ReleaseShared64\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\shared\
-copy ReleaseShared64TLS\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\tls\shared\
-copy ReleaseStatic64\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\static\
-copy ReleaseStatic64TLS\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\tls\static\
-
 echo "Linking to external libraries and examples for 64-bit release"
 pushd cpp\Windows_64
 mklink /j examples ..\examples
 mklink /j external ..\external
 popd
-
-echo "Clearing temporary 64bit librares"
-rd /s /q ./ReleaseShared64
-rd /s /q ./ReleaseShared64TLS
-rd /s /q ./ReleaseStatic64
-rd /s /q ./ReleaseStatic64TLS
 
 @REM Verify release
 call scripts/verifyReleaseWindows.bat || exit /b 1
---
+++ scripts/build-linux.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -e #abort the script at first failure
+
+# Disables printing security sensitive data to the logs
+set +x
+
+HZ_BIT_VERSION=$1
+HZ_LIB_TYPE=$2
+HZ_BUILD_TYPE=$3
+
+if [ "$4" == "WITH_COVERAGE" ]; then
+    if [ ${HZ_BUILD_TYPE} != Debug ]; then
+            echo "WITH_COVERAGE is requested. The build type should be Debug but it is provided as ${HZ_BUILD_TYPE}."
+            exit 1
+    fi
+    HZ_COVERAGE_STRING="-DHZ_CODE_COVERAGE=ON"
+fi
+
+if [ "$4" == "COMPILE_WITHOUT_SSL" ] || [ "$5" == "COMPILE_WITHOUT_SSL" ]; then
+    HZ_COMPILE_WITH_SSL=OFF
+else
+    HZ_COMPILE_WITH_SSL=ON
+fi
+
+BUILD_DIR=build${HZ_LIB_TYPE}${HZ_BIT_VERSION}${HZ_BUILD_TYPE}
+
+EXECUTABLE_NAME=clientTest_${HZ_LIB_TYPE}_${HZ_BIT_VERSION}
+
+echo HZ_BIT_VERSION=${HZ_BIT_VERSION}
+echo HZ_LIB_TYPE=${HZ_LIB_TYPE}
+echo HZ_BUILD_TYPE=${HZ_BUILD_TYPE}
+echo BUILD_DIR=${BUILD_DIR}
+echo EXECUTABLE_NAME=${EXECUTABLE_NAME}
+
+if [ "$4" == "WITH_COVERAGE" ]; then
+echo "Code coverage is ON. Cmake flag: ${HZ_COVERAGE_STRING}"
+fi
+
+# Let the submodule code be downloaded
+git submodule update --init
+
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+
+cd ${BUILD_DIR}
+
+echo "Running cmake to compose Makefiles for compilation."
+cmake .. -DHZ_LIB_TYPE=${HZ_LIB_TYPE} -DHZ_BIT=${HZ_BIT_VERSION} -DCMAKE_BUILD_TYPE=${HZ_BUILD_TYPE} ${HZ_COVERAGE_STRING} -DHZ_BUILD_TESTS=ON -DHZ_BUILD_EXAMPLES=ON -DHZ_COMPILE_WITH_SSL=${HZ_COMPILE_WITH_SSL}
+
+echo "Running make. Building the project."
+make -j 8 -l 4 VERBOSE=1  # run 8 jobs in parallel and a maximum load of 4
+if [ $? -ne 0 ]
+then
+    echo "Client compilation failed!!!"
+    exit 1
+fi
+
+cd ..
+
+exit 0
+
+
