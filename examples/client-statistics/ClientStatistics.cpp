052193785b5df6a805267b7214fd0209426da977
ihsandemir <ihsandemir@gmail.com>
2018-11-26 17:36:57 +0300

    Added the client statistics example.

---
+++ examples/client-statistics/ClientStatistics.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stdint.h>
+
+#include <hazelcast/client/HazelcastClient.h>
+
+using namespace hazelcast::client;
+
+int main() {
+    ClientConfig config;
+
+    config.setProperty(ClientProperties::STATISTICS_ENABLED, "true");
+
+    /**
+     * Collect and statistics every 5 seconds
+     */
+    config.setProperty(ClientProperties::STATISTICS_PERIOD_SECONDS, "5");
+
+    config.addNearCacheConfig(boost::shared_ptr<config::NearCacheConfig<int, int> >(new config::NearCacheConfig<int, int>("MyMap")));
+    hazelcast::client::HazelcastClient hz(config);
+
+    hazelcast::client::IMap<int, int> map = hz.getMap<int, int>("MyMap");
+    
+    map.put(2, 500);
+
+    // generate a near-cache miss
+    map.get(2);
+
+    // generate two near-cache hits
+    map.get(2);
+    map.get(2);
+
+    // sleep more than the statistics collection time and keep the client running
+    sleep(100);
+
+    // Statistics is now populated at the member side, so you can see it e.g. in Management Center.
+
+    std::cout << "Finished" << std::endl;
+
+    return 0;
+}
