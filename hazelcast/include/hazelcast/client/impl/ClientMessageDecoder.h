116b44ce1b5d04962bb3f7c5e57ae270334d7dc6
ihsandemir <ihsandemir@gmail.com>
2019-02-05 14:29:01 +0300

    Windows warning fix.

--- hazelcast/include/hazelcast/client/impl/ClientMessageDecoder.h
+++ hazelcast/include/hazelcast/client/impl/ClientMessageDecoder.h
@@ -20,6 +20,11 @@
 #include "hazelcast/client/protocol/ClientMessage.h"
 #include "hazelcast/client/serialization/pimpl/SerializationService.h"
 
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(push)
+#pragma warning(disable: 4251) //for dll export
+#endif
+
 namespace hazelcast {
     namespace client {
         namespace impl {
@@ -103,6 +108,10 @@ namespace hazelcast {
         }
     }
 }
+
+#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#pragma warning(pop)
+#endif
 
 #endif //HAZELCAST_CLIENT_IMPL_CLIENTMESSAGEDECODER_H_
 
