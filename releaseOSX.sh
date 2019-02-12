47640e544b0200acda2061296e329380d74c355d
ihsandemir <ihsandemir@gmail.com>
2019-02-12 10:27:23 +0300

    Changed Mac OS release scripts to run in parallel for shorter build and verification times.

--- releaseOSX.sh
+++ releaseOSX.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-set -e #abort the script at first faiure
+
+function removeBuildFolders {
+    rm -rf buildSTATIC64Release
+    rm -rf buildSHARED32Release
+    rm -rf buildSTATIC32Release
+    rm -rf buildSHARED64Release
+    rm -rf buildSTATIC64Release_SSL
+    rm -rf buildSHARED32Release_SSL
+    rm -rf buildSTATIC32Release_SSL
+    rm -rf buildSHARED64Release_SSL
+}
+
+function cleanup {
+    removeBuildFolders
+}
+
+trap cleanup EXIT
 
 rm -rf ./cpp
 
@@ -9,23 +25,36 @@ mkdir -p ./cpp/Mac_64/hazelcast/lib/tls
 cp -R hazelcast/include/hazelcast/* cpp/Mac_64/hazelcast/include/hazelcast/
 cp -R hazelcast/generated-sources/include/hazelcast/* cpp/Mac_64/hazelcast/include/hazelcast/
 
-scripts/build-linux.sh 64 STATIC Release COMPILE_WITHOUT_SSL
+scripts/build-linux.sh 64 STATIC Release COMPILE_WITHOUT_SSL &> STATIC_64_linux.txt &
+STATIC_64_pid=$!
+
+scripts/build-linux.sh 64 SHARED Release COMPILE_WITHOUT_SSL &> SHARED_64_linux.txt &
+SHARED_64_pid=$!
+
+scripts/build-linux.sh 64 STATIC Release &> STATIC_64_SSL_linux.txt &
+STATIC_64_SSL_pid=$!
+
+scripts/build-linux.sh 64 SHARED Release &> SHARED_64_SSL_linux.txt &
+SHARED_64_SSL_pid=$!
+
+FAIL=0
+wait ${STATIC_64_pid} || let "FAIL+=1"
+wait ${STATIC_64_SSL_pid} || let "FAIL+=1"
+wait ${SHARED_64_pid} || let "FAIL+=1"
+wait ${SHARED_64_SSL_pid} || let "FAIL+=1"
+
+if [ $FAIL -ne 0 ]; then
+    echo "$FAIL builds FAILED!!!"
+    exit $FAIL
+fi
+
 cp buildSTATIC64Release/libHazelcastClient* cpp/Mac_64/hazelcast/lib/
-rm -rf buildSTATIC64Release
 
-scripts/build-linux.sh 64 SHARED Release COMPILE_WITHOUT_SSL
 cp buildSHARED64Release/libHazelcastClient* cpp/Mac_64/hazelcast/lib/
-rm -rf buildSHARED64Release
 
-echo "Generating TLS enabled libraries"
+cp buildSTATIC64Release_SSL/libHazelcastClient* cpp/Mac_64/hazelcast/lib/tls/
 
-scripts/build-linux.sh 64 STATIC Release
-cp buildSTATIC64Release/libHazelcastClient* cpp/Mac_64/hazelcast/lib/tls/
-rm -rf buildSTATIC64Release
-
-scripts/build-linux.sh 64 SHARED Release
-cp buildSHARED64Release/libHazelcastClient* cpp/Mac_64/hazelcast/lib/tls/
-rm -rf buildSHARED64Release
+cp buildSHARED64Release_SSL/libHazelcastClient* cpp/Mac_64/hazelcast/lib/tls/
 
 echo "Copying external libraries and the examples"
 mkdir -p cpp/external
