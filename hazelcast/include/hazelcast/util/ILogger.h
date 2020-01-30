b948fd1fc88c89adcae42010872a49d344f30b7c
ihsandemir <ihsandemir@gmail.com>
2020-01-30 19:36:02 +0300

    updates

--- hazelcast/include/hazelcast/util/ILogger.h
+++ hazelcast/include/hazelcast/util/ILogger.h
@@ -23,6 +23,7 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <mutex>
 
 #if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
 // We need to include this header before easylogging++/easylogging++.h
