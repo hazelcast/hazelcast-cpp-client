ec15322b160c888280abecc4be229a67cc7fb62c
Viktor Zeliukov <RikeVoltz@yandex.ru>
2020-05-20 11:05:40 +0300

    Implement write/read UTFArray methods for PortableWriter/PortableReader (#596)

--- hazelcast/include/hazelcast/client/serialization/ClassDefinitionBuilder.h
+++ hazelcast/include/hazelcast/client/serialization/ClassDefinitionBuilder.h
@@ -76,6 +76,8 @@ namespace hazelcast {
 
                 ClassDefinitionBuilder& addDoubleArrayField(const std::string& fieldName);
 
+                ClassDefinitionBuilder& addUTFArrayField(const std::string& fieldName);
+
                 ClassDefinitionBuilder& addFloatArrayField(const std::string& fieldName);
 
                 ClassDefinitionBuilder& addShortArrayField(const std::string& fieldName);
