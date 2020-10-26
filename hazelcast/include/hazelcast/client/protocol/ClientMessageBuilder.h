2b6c8d5005c7b4ed83ff947c250f7d6311942771
ihsan demir <ihsan@hazelcast.com>
2020-10-26 19:06:10 +0300

    Add support to reading fragmented messages (#655)
    * Added the support to read fragmented messages.
    
    * Added a test for fragmentation. The test uses a binary generated via Java client. It uses response of IMap.getAll codec decodes 10 integer pairs as the response entries.

--- hazelcast/include/hazelcast/client/protocol/ClientMessageBuilder.h
+++ hazelcast/include/hazelcast/client/protocol/ClientMessageBuilder.h
@@ -13,68 +13,109 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * ClientMessageBuilder.h
- *
- *  Created on: Apr 10, 2015
- *      Author: ihsan
- */
-
 #pragma once
 
-#include "hazelcast/util/HazelcastDll.h"
-#include "hazelcast/client/protocol/ClientMessage.h"
-
 #include <unordered_map>
-#include <list>
-#include <stdint.h>
-#include <memory>
-#include <memory>
 
-#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-#pragma warning(push)
-#pragma warning(disable: 4251) //for dll export
-#endif
+#include "hazelcast/client/protocol/ClientMessage.h"
 
 namespace hazelcast {
     namespace util {
         class ByteBuffer;
     }
     namespace client {
-        namespace connection {
-            class Connection;
-        }
-
         namespace protocol {
-            class IMessageHandler;
-
-            class HAZELCAST_API ClientMessageBuilder {
-
+            template<typename MessageHandler>
+            class ClientMessageBuilder {
             public:
-                ClientMessageBuilder(connection::Connection &connection);
+                ClientMessageBuilder(MessageHandler &handler)  : message_handler_(handler) {}
 
-                virtual ~ClientMessageBuilder();
+                virtual ~ClientMessageBuilder() = default;
 
                 /**
                 * @returns true if message is completed, false otherwise
                 */
-                bool onData(util::ByteBuffer &buffer);
+                bool onData(util::ByteBuffer &buffer) {
+                    bool isCompleted = false;
+
+                    if (!message) {
+                        message.reset(new ClientMessage());
+                        is_final_frame = false;
+                        remaining_frame_bytes = 0;
+                    }
+
+                    if (message) {
+                        message->fillMessageFrom(buffer, is_final_frame, remaining_frame_bytes);
+                        isCompleted = is_final_frame && remaining_frame_bytes == 0;
+                        if (isCompleted) {
+                            //MESSAGE IS COMPLETE HERE
+                            message->wrap_for_read();
+                            isCompleted = true;
+
+                            if (message->is_flag_set(ClientMessage::UNFRAGMENTED_MESSAGE)) {
+                                //MESSAGE IS COMPLETE HERE
+                                message_handler_.handleClientMessage(std::move(message));
+                            } else {
+                                message->rd_ptr(ClientMessage::FRAGMENTATION_ID_OFFSET);
+                                auto fragmentation_id = message->get<int64_t>();
+                                auto flags = message->getHeaderFlags();
+                                message->drop_fragmentation_frame();
+                                if (ClientMessage::is_flag_set(flags, ClientMessage::BEGIN_FRAGMENT_FLAG)) {
+                                    // put the message into the partial messages list
+                                    addToPartialMessages(fragmentation_id, message);
+                                } else {
+                                    // This is the intermediate frame. Append at the previous message buffer
+                                    appendExistingPartialMessage(fragmentation_id, message,
+                                                                 ClientMessage::is_flag_set(flags,
+                                                                                            ClientMessage::END_FRAGMENT_FLAG));
+                                }
+                            }
+                        }
+                    }
+
+                    return isCompleted;
+                }
 
             private:
-                void addToPartialMessages(std::unique_ptr<ClientMessage> &message);
+                void addToPartialMessages(int64_t fragmentation_id, std::unique_ptr<ClientMessage> &message) {
+                    partialMessages[fragmentation_id] = std::move(message);
+                }
 
                 /**
                 * @returns true if message is completed, false otherwise
                 */
-                bool appendExistingPartialMessage(std::unique_ptr<ClientMessage> &message);
+                bool appendExistingPartialMessage(int64_t fragmentation_id, std::unique_ptr<ClientMessage> &msg,
+                                                  bool is_end_fragment) {
+                    bool result = false;
+
+                    auto found = partialMessages.find(fragmentation_id);
+                    if (partialMessages.end() != found) {
+                        found->second->append(std::move(msg));
+                        if (is_end_fragment) {
+                            // remove from message from map
+                            std::shared_ptr<ClientMessage> foundMessage(found->second);
+
+                            partialMessages.erase(found);
+
+                            foundMessage->wrap_for_read();
+                            message_handler_.handleClientMessage(foundMessage);
+
+                            result = true;
+                        }
+                    } else {
+                        // Should never be here
+                        assert(0);
+                    }
+
+                    return result;
+                }
 
                 typedef std::unordered_map<int64_t, std::shared_ptr<ClientMessage> > MessageMap;
 
                 MessageMap partialMessages;
 
                 std::unique_ptr<ClientMessage> message;
-                connection::Connection &connection;
+                MessageHandler &message_handler_;
 
                 bool is_final_frame;
                 size_t remaining_frame_bytes;
@@ -83,9 +124,6 @@ namespace hazelcast {
     }
 }
 
-#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-#pragma warning(pop)
-#endif
 
 
 
