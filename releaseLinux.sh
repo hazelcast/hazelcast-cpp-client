c21e589ee6cc38515555a51c8b7a73b45009dfca
ihsandemir <ihsandemir@gmail.com>
2020-11-12 00:26:42 +0300

    follow the logs of docker container.

--- releaseLinux.sh
+++ releaseLinux.sh
@@ -9,6 +9,12 @@ docker run -d --rm --name linux_32_bit_release_build -v `pwd`:/hazelcast-cpp-cli
 echo "Starting the docker build for 64-bit"
 docker run -d --rm --name linux_64_bit_release_build -v `pwd`:/hazelcast-cpp-client fedora_64 /bin/bash -l -c "cd hazelcast-cpp-client; scripts/release_linux_for_version.sh 64"
 
+echo "Following the logs of 32-bit build. The output will be at 32_bit_build_linux.txt"
+docker logs --follow linux_32_bit_release_build &> 32_bit_build_linux.txt &
+
+echo "Following the logs of 64-bit build. The output will be at 64_bit_build_linux.txt"
+docker logs --follow linux_64_bit_release_build &> 64_bit_build_linux.txt &
+
 echo "Waiting for 32-bit docker build to finish"
 docker wait linux_32_bit_release_build
 if [ $? -ne 0 ]; then
