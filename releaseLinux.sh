ea72d21dc957a6453280ad49fa14b11a9bfa3e17
ihsandemir <ihsandemir@gmail.com>
2020-11-12 00:28:03 +0300

    Use tee command to follow.

--- releaseLinux.sh
+++ releaseLinux.sh
@@ -10,10 +10,10 @@ echo "Starting the docker build for 64-bit"
 docker run -d --rm --name linux_64_bit_release_build -v `pwd`:/hazelcast-cpp-client fedora_64 /bin/bash -l -c "cd hazelcast-cpp-client; scripts/release_linux_for_version.sh 64"
 
 echo "Following the logs of 32-bit build. The output will be at 32_bit_build_linux.txt"
-docker logs --follow linux_32_bit_release_build &> 32_bit_build_linux.txt &
+docker logs --follow linux_32_bit_release_build 2>&1 | tee 32_bit_build_linux.txt &
 
 echo "Following the logs of 64-bit build. The output will be at 64_bit_build_linux.txt"
-docker logs --follow linux_64_bit_release_build &> 64_bit_build_linux.txt &
+docker logs --follow linux_64_bit_release_build 2>&1 | tee 64_bit_build_linux.txt &
 
 echo "Waiting for 32-bit docker build to finish"
 docker wait linux_32_bit_release_build
