4ca929b1b3dc5d793864c1ecdd7ee540d4457313
ihsandemir <ihsandemir@gmail.com>
2020-11-12 11:18:34 +0300

    Removed the top level verify script and put verification in the releaseWindows.bat script.

--- releaseWindows.bat
+++ releaseWindows.bat
@@ -20,6 +20,20 @@ xcopy /S /Q hazelcast\generated-sources\src\hazelcast\client\protocol\codec\*.h 
 xcopy /S /Q hazelcast\include\hazelcast\* cpp\Windows_64\hazelcast\include\hazelcast\
 xcopy /S /Q hazelcast\generated-sources\src\hazelcast\client\protocol\codec\*.h cpp\Windows_64\hazelcast\include\hazelcast\client\protocol\codec\
 
+echo "Copying the examples"
+mkdir cpp\examples\src
+xcopy examples\* cpp\examples\src\ /s
+
+echo "Linking to examples for 32-bit release"
+pushd cpp\Windows_32
+mklink /j examples ..\examples
+popd
+
+echo "Linking to examples for 64-bit release"
+pushd cpp\Windows_64
+mklink /j examples ..\examples
+popd
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 
 call scripts\build-windows.bat 32 STATIC Release COMPILE_WITHOUT_SSL || exit /b 1
@@ -42,6 +56,8 @@ rd /s /q buildSHARED64Release
 
 echo "Generating TLS enabled libraries"
 
+SET CURRENT_DIRECTORY=%cd%
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 
 call scripts\build-windows.bat 32 STATIC Release || exit /b 1
@@ -51,6 +67,10 @@ rd /s /q buildSTATIC32Release
 call scripts\build-windows.bat 32 SHARED Release || exit /b 1
 copy buildSHARED32Release\Release\HazelcastClient*  cpp\Windows_32\hazelcast\lib\tls\shared\
 rd /s /q buildSHARED32Release
+
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 32 STATIC || exit /b 1
+
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 32 SHARED || exit /b 1
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
@@ -62,19 +82,7 @@ call scripts\build-windows.bat 64 SHARED Release COMPILE_WITHOUT_SSL || exit /b 
 copy buildSHARED64Release\Release\HazelcastClient*  cpp\Windows_64\hazelcast\lib\tls\shared\
 rd /s /q buildSHARED64Release
 
-echo "Copying the examples"
-mkdir cpp\examples\src
-xcopy examples\* cpp\examples\src\ /s
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 64 STATIC || exit /b 1
 
-echo "Linking to examples for 32-bit release"
-pushd cpp\Windows_32
-mklink /j examples ..\examples
-popd
+call scripts\verifyReleaseWindowsSingleCase.bat "%CURRENT_DIRECTORY%\cpp" 64 SHARED || exit /b 1
 
-echo "Linking to examples for 64-bit release"
-pushd cpp\Windows_64
-mklink /j examples ..\examples
-popd
-
-@REM Verify release
-call scripts/verifyReleaseWindows.bat || exit /b 1
