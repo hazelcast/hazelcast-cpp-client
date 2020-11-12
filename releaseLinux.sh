c0e95c2ed7552faf3c2b0d0e87d117f2f94a6ea7
ihsandemir <ihsandemir@gmail.com>
2020-11-12 12:54:02 +0300

    Stop and remove any outstanding container if exists before start.

--- releaseLinux.sh
+++ releaseLinux.sh
@@ -27,6 +27,12 @@ trap cleanup EXIT
 docker build -t fedora_32 -f docker/hazelcast-fedora-i386.dockerfile docker
 docker build -t fedora_64 -f docker/hazelcast-fedora-x86_64.dockerfile docker
 
+# cleanup containers if exists before start
+docker stop linux_32_bit_release_build
+docker rm linux_32_bit_release_build
+docker stop linux_64_bit_release_build
+docker rm linux_64_bit_release_build
+
 echo "Starting the docker build for 32-bit"
 docker run -d --rm --name linux_32_bit_release_build -v `pwd`:/hazelcast-cpp-client fedora_32 /bin/bash -l -c "cd hazelcast-cpp-client; scripts/release_linux_for_version.sh 32"
 
