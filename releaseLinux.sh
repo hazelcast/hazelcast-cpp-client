f7eb5657463f3adab8dda3e2507a8897f62b4216
ihsandemir <ihsandemir@gmail.com>
2020-11-25 17:02:09 +0300

    fix

--- releaseLinux.sh
+++ releaseLinux.sh
@@ -37,8 +37,8 @@ cp -r examples ${relative_install_dir}/
 
 trap cleanup EXIT
 
-docker build -t fedora22_32 -f docker/hazelcast-fedora22-i386.dockerfile docker
-docker build -t fedora_64 -f docker/hazelcast-fedora-x86_64.dockerfile docker
+docker build -t fedora_32 -f docker/hazelcast-fedora-i386.dockerfile docker
+docker build -t fedora22_64 -f docker/hazelcast-fedora22-x86_64.dockerfile docker
 
 # cleanup containers if exists before start
 docker stop linux_32_bit_release_build
@@ -47,10 +47,10 @@ docker stop linux_64_bit_release_build
 docker rm linux_64_bit_release_build
 
 echo "Starting the docker build for 32-bit"
-docker run -d --name linux_32_bit_release_build -v `pwd`:/hazelcast-cpp-client -w /hazelcast-cpp-client fedora22_32 /bin/bash -l -c "scripts/build_linux_release_libraries.sh 32 ${relative_install_dir}"
+docker run -d --name linux_32_bit_release_build -v `pwd`:/hazelcast-cpp-client -w /hazelcast-cpp-client fedora_32 /bin/bash -l -c "scripts/build_linux_release_libraries.sh 32 ${relative_install_dir}"
 
 echo "Starting the docker build for 64-bit"
-docker run -d --name linux_64_bit_release_build -v `pwd`:/hazelcast-cpp-client -w /hazelcast-cpp-client fedora_64 /bin/bash -l -c "scripts/build_linux_release_libraries.sh 64 ${relative_install_dir}"
+docker run -d --name linux_64_bit_release_build -v `pwd`:/hazelcast-cpp-client -w /hazelcast-cpp-client fedora22_64 /bin/bash -l -c "scripts/build_linux_release_libraries.sh 64 ${relative_install_dir}"
 
 echo "Waiting for 32-bit docker build to finish"
 result=`docker wait linux_32_bit_release_build`
