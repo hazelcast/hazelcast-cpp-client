0c8e5d92b2a27cbf00d5f39a971dce0d4f2a7d34
ihsandemir <ihsandemir@gmail.com>
2020-11-26 01:03:08 +0300

    Added the API documentation to the repository.

---
+++ docs/search/classes_b.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['named_5fpredicate',['named_predicate',['../classhazelcast_1_1client_1_1query_1_1named__predicate.html',1,'hazelcast::client::query']]],
+  ['near_5fcache_5fconfig',['near_cache_config',['../classhazelcast_1_1client_1_1config_1_1near__cache__config.html',1,'hazelcast::client::config']]],
+  ['near_5fcache_5fstats',['near_cache_stats',['../classhazelcast_1_1client_1_1monitor_1_1near__cache__stats.html',1,'hazelcast::client::monitor']]],
+  ['nearcache',['NearCache',['../classhazelcast_1_1client_1_1internal_1_1nearcache_1_1_near_cache.html',1,'hazelcast::client::internal::nearcache']]],
+  ['nearcachemanager',['NearCacheManager',['../classhazelcast_1_1client_1_1internal_1_1nearcache_1_1_near_cache_manager.html',1,'hazelcast::client::internal::nearcache']]],
+  ['nearcacherecord',['NearCacheRecord',['../classhazelcast_1_1client_1_1internal_1_1nearcache_1_1_near_cache_record.html',1,'hazelcast::client::internal::nearcache']]],
+  ['not_5fequal_5fpredicate',['not_equal_predicate',['../classhazelcast_1_1client_1_1query_1_1not__equal__predicate.html',1,'hazelcast::client::query']]],
+  ['not_5fpredicate',['not_predicate',['../classhazelcast_1_1client_1_1query_1_1not__predicate.html',1,'hazelcast::client::query']]]
+];
