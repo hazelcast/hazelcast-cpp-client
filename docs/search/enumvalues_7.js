8cc93fa2e6e7780f2dcd1648697a7bab2fb35c7d
ihsandemir <ihsandemir@gmail.com>
2020-11-26 17:50:17 +0300

    Removed the docs folder since we will put these into a new branch origin/gh-pages.

--- docs/search/enumvalues_7.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['object',['OBJECT',['../structhazelcast_1_1client_1_1config_1_1index__config_1_1bitmap__index__options.html#a5280c8dc2ffcfc6c682b144ab783c019a3f212977a959e0916e85db2cc8588703',1,'hazelcast::client::config::index_config::bitmap_index_options']]],
-  ['off',['OFF',['../classhazelcast_1_1client_1_1config_1_1client__connection__strategy__config.html#ae450cea7d3d15a041eb0dceaf8a9945eafed940f22a1a437763da6b3f9c5b4fbc',1,'hazelcast::client::config::client_connection_strategy_config']]],
-  ['on',['ON',['../classhazelcast_1_1client_1_1config_1_1client__connection__strategy__config.html#ae450cea7d3d15a041eb0dceaf8a9945ea5f0c35885635ec6a80f61a16e22c4ec8',1,'hazelcast::client::config::client_connection_strategy_config']]]
-];
--- docs/search/variables_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['last_5faccess_5ftime',['last_access_time',['../classhazelcast_1_1client_1_1entry__view.html#a88e36f1bfb1a86ac8ad6f522905642db',1,'hazelcast::client::entry_view']]],
-  ['last_5fstored_5ftime',['last_stored_time',['../classhazelcast_1_1client_1_1entry__view.html#a65bdeba94e9e3a26c9156af46d286627',1,'hazelcast::client::entry_view']]],
-  ['last_5fupdate_5ftime',['last_update_time',['../classhazelcast_1_1client_1_1entry__view.html#a786fb9b9d759b6776cc34d6700c54a80',1,'hazelcast::client::entry_view']]]
-];
