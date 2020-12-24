85260b7e663c099b0de97fa6c9e32d0efb3edfb9
Yunus Emre İnci <yemreinci0@gmail.com>
2020-12-24 15:56:09 +0300

    Add bit_version for verify-install script

--- scripts/verify-installation-unix.sh
+++ scripts/verify-installation-unix.sh
@@ -5,6 +5,8 @@
 #
 # This environment variables are the parameters to this script:
 # - BUILD_DIR : build directory
+# - BIT_VERSION : target platform architecture (32 or 64)
+# - CXXFLAGS : additional compiler flags
 #
 # Command line arguments are forwarded to CMake.
 #
@@ -12,11 +14,19 @@
 # exit if a command returns non-zero status
 set -e
 
+# set -m32 or -m64 if a BIT_VERSION is given
+if [ -n "$BIT_VERSION" ]; then
+  CXXFLAGS="$CXXFLAGS -m$BIT_VERSION"
+fi
+
 # remove the given build directory if already exists
 if [ -d "$BUILD_DIR" ]; then
   echo "Given build directory $BUILD_DIR exists. Removing for a clean build."
   rm -rf $BUILD_DIR
 fi
+
+# export flags variable to be used by CMake
+export CXXFLAGS
 
 echo "Configuring..."
 cmake -S ./examples -B "$BUILD_DIR" "$@"
