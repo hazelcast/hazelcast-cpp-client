d0b65ca8b66bf265fdbccfefb770db5d22191443
Yunus Emre Inci <yemreinci0@gmail.com>
2020-12-25 16:32:47 +0300

    Use generate_export_header, minor renaming.

--- hazelcast/include/hazelcast/util/hazelcast_dll.h
+++ hazelcast/include/hazelcast/util/hazelcast_dll.h
@@ -15,28 +15,8 @@
  */
 #pragma once
 
-#if  defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-    #if !(defined(HAZELCAST_USE_STATIC) || defined(HAZELCAST_USE_SHARED))
-        #ifndef HAZELCAST_EXPORTS
-            #error Please define compilation flag HAZELCAST_USE_STATIC or HAZELCAST_USE_SHARED.
-        #endif
-    #endif
+#include <hazelcast/export.h>
 
-    #if defined(HAZELCAST_USE_STATIC)
-        #define HAZELCAST_API
-    #elif defined(HAZELCAST_USE_SHARED)
-        #define HAZELCAST_API __declspec(dllimport)
-    #else //HAZELCAST_EXPORTS case
-        #if HAZELCAST_EXPORTS == 1
-            #define HAZELCAST_API __declspec(dllexport)
-        #else
-            #define HAZELCAST_API
-        #endif
-    #endif
-
-#else
-    #define HAZELCAST_API
- #endif
 namespace hazelcast{
     typedef unsigned char byte;
 }
