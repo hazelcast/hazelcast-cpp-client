145825e81ec60a91308ea1f0c671aee32510db25
yemreinci <18687880+yemreinci@users.noreply.github.com>
2021-03-10 11:00:29 +0300

    API-125 Add Github Actions workflows for nightly macOS and Linux builds (#819)
    * Add nightly GA workflows for Mac, Linux 32 and 64
    
    * Make scripts CMake 3.10 compatible
    
    * print cmake args in build-unix.sh
    
    * Print config summary in cmakelists

---
+++ scripts/install-thrift.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Installs Apache Thrift from source
+# usage ./install-thrift.sh <version>
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "usage: $0 <version>"
+    exit 1
+fi
+
+curl --silent -Lo thrift-$1.tar.gz https://archive.apache.org/dist/thrift/$1/thrift-$1.tar.gz
+tar xzf thrift-$1.tar.gz
+cd thrift-$1/build
+cmake .. -DBUILD_COMPILER=OFF -DBUILD_TESTING=OFF -DBUILD_TUTORIALS=OFF -DBUILD_LIBRARIES=ON \
+         -DBUILD_CPP=ON -DBUILD_AS3=OFF -DBUILD_C_GLIB=OFF -DBUILD_JAVA=OFF -DBUILD_PYTHON=OFF \
+         -DBUILD_HASKELL=OFF -DWITH_OPENSSL=OFF -DWITH_LIBEVENT=OFF -DWITH_ZLIB=OFF \
+         -DWITH_QT5=OFF
+cmake --build . --target install
