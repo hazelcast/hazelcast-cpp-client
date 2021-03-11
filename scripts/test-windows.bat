32b915bcc0dc5693b3e58b8b369bf267608152b6
Yunus Emre Inci <yemreinci0@gmail.com>
2021-03-11 12:41:52 +0300

    ssl shared wo aws

--- scripts/test-windows.bat
+++ scripts/test-windows.bat
@@ -51,7 +51,7 @@ echo "Starting the client test now."
 set PATH=%BUILD_DIR%\%BUILD_CONFIGURATION%;%BUILD_DIR%\bin\%BUILD_CONFIGURATION%;%PATH%
 
 echo %TEST_EXECUTABLE%
-%TEST_EXECUTABLE% --gtest_output="xml:CPP_Client_Test_Report.xml" --gtest_filter=-*ReplicatedMap*
+%TEST_EXECUTABLE% --gtest_output="xml:CPP_Client_Test_Report.xml" --gtest_filter=-*AwsClient*
 set result=%errorlevel%
 
 taskkill /T /F /FI "WINDOWTITLE eq hazelcast-remote-controller"
