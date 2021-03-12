abffa34e2d207b2da1ae5f56e9719418ccd1f3dd
Yunus Emre İnci <yemreinci0@gmail.com>
2021-03-12 15:44:09 +0300

    more

--- hazelcast/test/src/main.cpp
+++ hazelcast/test/src/main.cpp
@@ -74,6 +74,10 @@ int main(int argc, char **argv) {
 
     testing::InitGoogleTest(&argc, argv);
 
-    return RUN_ALL_TESTS();
+    int retcode = RUN_ALL_TESTS();
+
+    std::cerr << "retcode: " << recode << std::endl;
+
+    return retcode;
 }
 
