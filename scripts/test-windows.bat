a584ebc0eeb1daf556eb8f760919ba93d90cf807
Yunus Emre İnci <yemreinci0@gmail.com>
2021-05-11 23:00:57 +0300

    revert redundant changes

--- scripts/test-windows.bat
+++ scripts/test-windows.bat
@@ -51,13 +51,9 @@ echo "Starting the client test now."
 set PATH=%BUILD_DIR%\%BUILD_CONFIGURATION%;%BUILD_DIR%\bin\%BUILD_CONFIGURATION%;%PATH%
 
 echo %TEST_EXECUTABLE%
-%TEST_EXECUTABLE% --gtest_output="xml:CPP_Client_Test_Report.xml" --gtest_filter=-*AwsClient*
+%TEST_EXECUTABLE% --gtest_output="xml:CPP_Client_Test_Report.xml"
 set result=%errorlevel%
-echo result = %result%
-
-type rclog.txt
 
 taskkill /T /F /FI "WINDOWTITLE eq hazelcast-remote-controller"
-
 
 exit /b %result%
