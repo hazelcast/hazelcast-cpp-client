8acdcf471af67ecfb8e888d3882763b4bc0ecbe8
ihsan <ihsandemir@gmail.com>
2021-06-25 16:06:27 +0300

    test fix + review comment fix.

--- hazelcast/test/src/HazelcastTests2.cpp
+++ hazelcast/test/src/HazelcastTests2.cpp
@@ -172,7 +172,7 @@ namespace hazelcast {
                 std::string timeString = hazelcast::util::StringUtil::time_to_string(
                         std::chrono::steady_clock::now());
                 //expected format is "%Y-%m-%d %H:%M:%S.%f" it will be something like 2018-03-20 15:36:07.280
-                ASSERT_EQ((size_t) 23, timeString.length());
+                ASSERT_LE((size_t) 21, timeString.length());
                 ASSERT_EQ(timeString[0], '2');
                 ASSERT_EQ(timeString[1], '0');
                 ASSERT_EQ(timeString[4], '-');
@@ -192,7 +192,7 @@ namespace hazelcast {
                 ASSERT_GE(actualDuration, parkDurationNanos);
             }
 
-            TEST_F (ClientUtilTest, print_older_stead_clock_time) {
+            TEST_F (ClientUtilTest, print_older_steady_clock_time) {
                 auto now = std::chrono::steady_clock::now();
                 auto time_str = hazelcast::util::StringUtil::time_to_string(now);
                 ASSERT_EQ(std::string::npos, time_str.substr(time_str.find_last_of('.')).find('-'));
