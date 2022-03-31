06a0baadf0aefa9976b9c3707be877ebf116df4c
sancar <sancar@hazelcast.com>
2022-03-31 14:17:38 +0300

    further compile fix

--- hazelcast/include/hazelcast/client/serialization/pimpl/compact.i.h
+++ hazelcast/include/hazelcast/client/serialization/pimpl/compact.i.h
@@ -16,6 +16,7 @@
 #pragma once
 
 #include "hazelcast/client/serialization/pimpl/compact.h"
+#include "hazelcast/util/finally.h"
 
 namespace hazelcast {
 namespace client {
@@ -38,27 +39,15 @@ get_offset(serialization::object_data_input& in,
 }
 
 } // namespace offset_reader
-
-class HAZELCAST_API restore_position_at_exit
-{
-public:
-    restore_position_at_exit(serialization::object_data_input& input)
-      : input(input)
-      , pos(input.position())
-    {}
-    ~restore_position_at_exit() { input.position(pos); }
-
-private:
-    serialization::object_data_input& input;
-    int pos;
-};
 } // namespace pimpl
 template<typename T>
 boost::optional<T>
 compact_reader::get_variable_size(
   const pimpl::field_descriptor& field_descriptor)
 {
-    pimpl::restore_position_at_exit _(object_data_input);
+    int current_pos = object_data_input.position();
+    util::finally set_position_back(
+      [this, current_pos]() { this->object_data_input.position(current_pos); });
     int pos = read_var_size_position(field_descriptor);
     if (pos == pimpl::offset_reader::NULL_OFFSET) {
         return boost::none;
