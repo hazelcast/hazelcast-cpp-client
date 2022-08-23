c0ed68e0507f1771f0783354b9bedb382626fcc9
hakanaktas0 <104701041+hakanaktas0@users.noreply.github.com>
2022-08-23 15:09:33 +0300

    Assertion failure is fixed

--- hazelcast/src/hazelcast/cp/cp_impl.cpp
+++ hazelcast/src/hazelcast/cp/cp_impl.cpp
@@ -82,6 +82,10 @@ proxy_session_manager::create_new_session(const raft_group_id& group_id)
     auto response = request_new_session(group_id);
     auto result = sessions_.emplace(
       group_id, session_state{ response.id, response.ttl_millis });
+    if(!result.second){
+        sessions_.erase(result.first);
+        result = sessions_.emplace(group_id, session_state{response.id, response.ttl_millis});
+    }
     assert(result.second);
     auto session =
       sessions_
