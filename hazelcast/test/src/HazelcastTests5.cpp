cf1b954dc4ada42185cb0d9bb2984ec8278427b6
Ali Keles <83716339+akeles85@users.noreply.github.com>
2023-02-10 17:31:07 +0300

    test fix

--- hazelcast/test/src/HazelcastTests5.cpp
+++ hazelcast/test/src/HazelcastTests5.cpp
@@ -1889,8 +1889,8 @@ TEST_P(ClientMapTest, testValuesWithpaging_predicate)
     std::vector<employee> result2 =
       employees_->values<int, employee>(predicate4).get();
     ASSERT_EQ(2, (int)result.size());
-    EXPECT_EQ(empl3, result[0]);
-    EXPECT_EQ(empl2, result[1]);
+    EXPECT_EQ(empl2, result2[0]);
+    EXPECT_EQ(empl3, result2[1]);
    
 }
 
