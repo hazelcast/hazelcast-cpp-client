aa133f85e8d20060a20d448daf8d290ec5fefec0
Ali Keles <83716339+akeles85@users.noreply.github.com>
2023-02-21 09:03:14 +0300

    Ssl version 3 [API-1872] (#1098)
    * SSLv3 EVP API instead of HMAC
    
    * Update nightly-macos-x86_64.yml
    
    * FIPS API Change
    
    * fips test updated
    
    * Revert "Update nightly-macos-x86_64.yml"
    
    This reverts commit b0da676c16385966957ed74c14c9b2ceb45ef32e.
    
    * key sign added

--- hazelcast/src/hazelcast/client/discovery.cpp
+++ hazelcast/src/hazelcast/client/discovery.cpp
@@ -262,6 +262,20 @@ ec2_request_signer::hmac_sh_a256_bytes(const void* key_buffer,
                                        size_t data_len,
                                        unsigned char* hash) const
 {
+
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    //https://www.openssl.org/docs/man3.0/man7/migration_guide.html
+    unsigned int len = 32;
+    EVP_MD_CTX *mdctx;
+    mdctx = EVP_MD_CTX_new();
+    EVP_PKEY *skey = NULL;
+    skey = EVP_PKEY_new_mac_key(EVP_PKEY_HMAC, NULL, (const unsigned char *)key_buffer, key_len);
+    EVP_DigestSignInit(mdctx, NULL, EVP_sha256(), NULL, skey);
+    EVP_DigestSignUpdate(mdctx, data, data_len);
+    EVP_DigestSignFinal(mdctx, hash, (size_t *)&len);
+    EVP_PKEY_free(skey);
+    EVP_MD_CTX_free(mdctx);    
+#else
 #if OPENSSL_VERSION_NUMBER >= 0x10100000L
     HMAC_CTX* hmac = HMAC_CTX_new();
 #else
@@ -279,6 +293,7 @@ ec2_request_signer::hmac_sh_a256_bytes(const void* key_buffer,
 #else
     HMAC_CTX_cleanup(hmac);
     delete hmac;
+#endif
 #endif
 
     return len;
