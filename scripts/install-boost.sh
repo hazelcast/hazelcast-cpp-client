b38297524845dd6479eee9a19d9fa554ccfdfab4
Jack Green <jack.green@hazelcast.com>
2025-02-19 14:01:37 +0000

    Fix `boost` installation script (#1257)
    The [coverage action is
    failing](https://github.com/hazelcast/hazelcast-cpp-client/actions/runs/13248710933/job/36981292995)
    because the `boost` installation mirror [is
    dead](https://landing.jfrog.com/reactivate-server/boostorg).
    
    Changes:
    - update installation link to use `boost` archive
    - refactored script
       - use long-form command names
       - fix shellcheck warnings
       - avoid writing temporary download files to disk
    
    Example action [showing `boost` installation
    success](https://github.com/hazelcast/hazelcast-cpp-client/actions/runs/13249150779/job/36982695856).

--- scripts/install-boost.sh
+++ scripts/install-boost.sh
@@ -3,18 +3,17 @@
 # Installs Boost from source
 # usage: ./install-boost.sh <version>
 
-set -e
+set -o errexit ${RUNNER_DEBUG:+-x}
 
-if [ "$#" -ne 1 ]; then
+if [[ "$#" -ne 1 ]]; then
     echo "usage: $0 <version>"
     exit 1
 fi
 
 TARBALL_NAME=boost_$(echo "$1" | tr . _)
 
-curl --silent -Lo $TARBALL_NAME.tar.gz https://boostorg.jfrog.io/artifactory/main/release/$1/source/$TARBALL_NAME.tar.gz
-tar xzf $TARBALL_NAME.tar.gz
-cd $TARBALL_NAME
+curl --silent --location "https://archives.boost.io/release/${1}/source/${TARBALL_NAME}.tar.gz" | tar xzf -
+pushd "${TARBALL_NAME}"
 ./bootstrap.sh
 ./b2 --with-thread --with-chrono install
-cd ..
+popd
