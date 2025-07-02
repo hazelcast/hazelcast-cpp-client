18d1ba59455cda76d62e7ed00b4c14ee916587a8
Jack Green <JackPGreen@Gmail.com>
2025-07-02 11:31:42 +0100

    Use `https`

--- scripts/install-thrift.sh
+++ scripts/install-thrift.sh
@@ -13,7 +13,7 @@ fi
 # Ideally we would download directly from Apache:
 # https://www.apache.org/dyn/closer.lua/thrift/$1/thrift-$1.tar.gz?action=download
 # But this has intermittent failures in GitHub Actions
-curl --fail --silent --show-error --location "http://deb.debian.org/debian/pool/main/t/thrift/thrift_$1.orig.tar.gz" | tar xzf -
+curl --fail --silent --show-error --location "https://deb.debian.org/debian/pool/main/t/thrift/thrift_$1.orig.tar.gz" | tar xzf -
 pushd thrift-$1/build
 cmake .. -DBUILD_COMPILER=OFF -DBUILD_TESTING=OFF -DBUILD_TUTORIALS=OFF -DBUILD_LIBRARIES=ON \
          -DBUILD_CPP=ON -DBUILD_AS3=OFF -DBUILD_C_GLIB=OFF -DBUILD_JAVA=OFF -DBUILD_PYTHON=OFF \
