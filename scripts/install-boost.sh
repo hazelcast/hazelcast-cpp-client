5695aa3f635f3cf6c9cb5c71d801cafa6bba11bc
Jack <jack.green@hazelcast.com>
2025-10-15 09:48:55 +0100

    Document `b2` paramaters
    There was no clear description of what the `b2` parameters did.
    
    [Related discussion](https://github.com/hazelcast/hazelcast-cpp-client/pull/1322#discussion_r2419330300).

--- scripts/install-boost.sh
+++ scripts/install-boost.sh
@@ -15,5 +15,8 @@ TARBALL_NAME=boost_$(echo "$1" | tr . _)
 curl --fail --silent --show-error --location "https://archives.boost.io/release/${1}/source/${TARBALL_NAME}.tar.gz" | tar xzf -
 pushd "${TARBALL_NAME}"
 ./bootstrap.sh
+# Include:
+# - Thread - https://www.boost.org/libs/thread/
+# - Atomic - https://www.boost.org/libs/atomic/
 ./b2 --with-thread --with-chrono install
 popd
