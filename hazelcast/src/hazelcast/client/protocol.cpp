f5e38b25ff224478e0cab0d849a798a2787449c5
ihsan <ihsandemir@gmail.com>
2025-10-15 12:45:50 +0300

    Fixed build problems with latest Boost library versions.

--- hazelcast/src/hazelcast/client/protocol.cpp
+++ hazelcast/src/hazelcast/client/protocol.cpp
@@ -376,7 +376,8 @@ operator<<(std::ostream& os, const ClientMessage& msg)
 void
 ClientMessage::set(unsigned char* /* memory */, boost::uuids::uuid uuid)
 {
-    std::memcpy(wr_ptr(uuid.size()), uuid.data, uuid.size());
+    assert(uuid.size() == util::Bits::UUID_SIZE_IN_BYTES);
+    std::memcpy(wr_ptr(uuid.size()), &uuid.data[0], uuid.size());
 }
 
 void
