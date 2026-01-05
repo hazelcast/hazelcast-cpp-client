f1e567ff0c02d29cf069848235e862041773049a
ihsan <ihsandemir@gmail.com>
2026-01-05 10:52:20 +0300

    review comment fixes.

--- hazelcast/include/hazelcast/client/impl/metrics/metrics_compressor.h
+++ hazelcast/include/hazelcast/client/impl/metrics/metrics_compressor.h
@@ -28,11 +28,6 @@
 #include "hazelcast/util/byte.h"
 #include "hazelcast/util/export.h"
 
-#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-#pragma warning(push)
-#pragma warning(disable : 4251) // for dll export
-#endif
-
 namespace hazelcast {
 namespace client {
 namespace impl {
@@ -76,7 +71,3 @@ private:
 } // namespace impl
 } // namespace client
 } // namespace hazelcast
-
-#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-#pragma warning(pop)
-#endif
